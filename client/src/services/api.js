82ce0057e878ac6e1f58d045da132c3674fe4405
vanamprasad42 <prasadvanam42@gmail.com>
2026-08-01 13:04:15 +0530

    Update API base URL to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://realestate-management-property-portal.onrender.com/api',
 });
 
 // Add a request interceptor to attach JWT token
